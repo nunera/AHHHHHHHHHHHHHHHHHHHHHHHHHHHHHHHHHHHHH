fc79cc57bb20ba96c4546cb7b082b633810b0d03
nunera <67439647+nunera@users.noreply.github.com>
2023-02-15 14:48:31 -0600

    Create OlympicMedal.py

---
+++ OlympicMedal.py
@@ -0,0 +1,13 @@
+def Get_Winnings(m):
+    try:
+        if int(m) <= 5 and int(m) > 0:
+            prize = int(m) * 75000
+        else:
+            prize = "Invalid"
+    except:
+        prize = "Invalid"
+    return(prize)
+    
+inp = str(input("Enter Gold Medals Won: "))
+print("Your prize money is: "+str(Get_Winnings(inp)))
+
